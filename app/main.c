6444b8c3f329029710053e86d1e356e456537f1d
vidais <antonio.vidais@tecnico.ulisboa.pt>
2025-02-09 19:24:02 +0000

    3rd task

--- app/main.c
+++ app/main.c
@@ -5,13 +5,13 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
 
   // Wait for user input
   char input[100];
 
   while (true){
+    
+    printf("$ ");
     fgets(input, 100, stdin);
   
     input[strlen(input) - 1] = '\0';
