1e997efab031b574e657959296d9aef5202afae9
vidais <antonio.vidais@tecnico.ulisboa.pt>
2025-02-09 19:25:55 +0000

    4th task

--- app/main.c
+++ app/main.c
@@ -13,9 +13,13 @@ int main() {
     
     printf("$ ");
     fgets(input, 100, stdin);
-  
-    input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
+    
+    if(strcmp(input, "exit 0")){
+      return 0;
+    } else {
+      input[strlen(input) - 1] = '\0';
+      printf("%s: command not found\n", input);
+    }
   }
 
   return 0;
