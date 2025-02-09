756acbf546f387406b98902152994b27f10677cd
vidais <antonio.vidais@tecnico.ulisboa.pt>
2025-02-09 19:18:38 +0000

    2nd task

--- app/main.c
+++ app/main.c
@@ -10,5 +10,9 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
+  
   return 0;
 }
