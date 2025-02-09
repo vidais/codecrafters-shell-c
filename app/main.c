dd1390a966a8590c6278131ac60c634c980f37d7
vidais <antonio.vidais@tecnico.ulisboa.pt>
2025-02-09 19:36:18 +0000

    4th task

--- app/main.c
+++ app/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
   // Flush after every printf
@@ -15,13 +16,14 @@ int main() {
     printf("$ ");
     fgets(input, 100, stdin);
     
+    input[strlen(input) - 1] = '\0';
+    
     if(strcmp(input, "exit")==true){
 
-      return 0;
+      exit(0);
 
     } else {
 
-      input[strlen(input) - 1] = '\0';
       printf("%s: command not found\n", input);
     
     }
